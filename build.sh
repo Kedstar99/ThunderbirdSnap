eb17a19ec74141cefad85a808ef76ced1a4e6c4b
Krish De Souza <K.DeSouza@Outlook.com>
2019-11-13 01:37:57 +0000

    Removed Docker and moved to more modern snapcraft design, almost got enigmail working too

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-rm thunderbird.snap
-docker build -t thunderbird -f ./DockerFile .
-container=$(docker container create thunderbird)
-echo "$container"
-docker cp "$container":/thunderbird/thunderbird.snap .
-echo "To install the snap run 'sudo snap install thunderbird.snap --dangerous --jailmode'"
